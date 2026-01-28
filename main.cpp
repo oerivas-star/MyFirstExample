ec483adede5e3cc031a89cbd8f42eaaecc961d38
oerivas-star <oerivas@csuchico.edu>
2026-01-28 11:42:47 -0800

    prevents divide by 0, resolves #61

--- main.cpp
+++ main.cpp
@@ -12,7 +12,12 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
+    
+    if (y == 0){
+	std::cout << "Dviding by zero is not a number.\n";
+    } else {
+	std::cout << "Division: " << x / y << std::endl;
+    }
     std::cout << "Remainder: " << x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
